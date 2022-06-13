a1fbeba3fa5b03766b5d909b57645c0a364fc4dd
Oleksandr Dzebovski <dzebovski@gmail.com>
2022-06-13 17:31:35 +0300

    Creating props data

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,10 +1,40 @@
 //
 import Card from "./Card"
+// Images
+import img1 from "./katie-zaferis.jpg"
+import img2 from "./wedding.jpg"
+import img3 from "./bike.jpg"
 
 export default function CardsContainer() {
     return (
         <div className="container cards">
-            <Card />
+            <Card
+                status = 'sold out'
+                img = {img1}
+                rating = {5.0}
+                reviews = {6}
+                country = 'USA'
+                text = 'Life lessons with Katie Zaferes'
+                price = {136}
+            />
+            <Card
+                status = 'online'
+                img = {img2}
+                rating = {4.2}
+                reviews = {42}
+                country = 'Online'
+                text = 'Learn wedding photography'
+                price = {125}
+            />
+            <Card
+                status = 'sale'
+                img = {img3}
+                rating = {4.8}
+                reviews = {2}
+                country = 'Alaska'
+                text = 'Group Mountain Biking'
+                price = {50}
+            />
         </div>
     )
 }
